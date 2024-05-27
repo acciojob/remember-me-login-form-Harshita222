e6060f766ef333ba1a7094b08dc69a6fca20c904
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-27 23:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const btn = document.getElementById('submit');
+     btn.addEventListener("click" , eventDetails);
+
+function eventDetails(events) {
+	event.preventDefault();
+
+	const user = document.getElementById('username');
+	const pass = document.getElementById('password');
+	const  rememberMe = document.getElementById('checkbox').checked;
+
+ if (rememberMe == true) {
+ 	localStorage.setItem('username',user.value);
+	 localStorage.setItem('password',pass.value)
+ } else{
+	 localStorage.removeItem('username');
+	 
+	 localStorage.removeItem('password');
+ }
+
+
+	alert(`Logged in as ${user.value}`);
+}
+	// alert(`Logged in as ${username}`)
