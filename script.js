8e5b1afa77146e11ab1de0500b58d6e186db6f24
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-28 00:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,35 +11,28 @@ function eventDetails(events) {
     const existingUserDiv = document.getElementById('existingUserDiv');
     const existingUserButton = document.getElementById('existing');
 
- if (rememberMe == true) {
+ if (rememberMe ) {
  	localStorage.setItem('username',user.value);
 	 localStorage.setItem('password',pass.value)
- } else{
-	 localStorage.removeItem('username');
+ } 
+	onst existingButton = document.createElement('button');
+        existingButton.id = 'existing';
+        existingButton.innerText = 'Login as existing user';
+        existingButton.addEventListener('click', function() {
+            alert(`Logged in as ${user.value}`);
+        });
+
+        document.getElementById('form-group').appendChild(existingButton);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+ // else{
+	//  localStorage.removeItem('username');
 	 
-	 localStorage.removeItem('password');
- }
+	//  localStorage.removeItem('password');
+ // }
 
 
 	alert(`Logged in as ${user.value}`);
 }
-	window.onload = function() {
-    const savedUsername = localStorage.getItem('username');
-    const savedPassword = localStorage.getItem('password');
-
-    if (savedUsername !== null && savedPassword !== null) {
-        const existingButton = document.createElement('button');
-        existingButton.id = 'existing';
-        existingButton.innerText = 'Login as existing user';
-        existingButton.addEventListener('click', function() {
-            alert(`Logged in as ${savedUsername}`);
-        });
-
-        document.getElementById('form-group').appendChild(existingButton);
-    }
-};
- // existingUserButton.addEventListener('click', () => {
- //        const username = localStorage.getItem('username');
- //        alert(`Logged in as ${username}`);
- //    });
-// alert(`Logged in as ${username}`)
